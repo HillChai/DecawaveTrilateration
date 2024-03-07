5713ba1e8180e7a3f79d5f444fe5182630838778
ccc <hillchai2023@gmail.com>
2024-03-07 22:12:43 +0800

    Found .c, .h, .so, .dll...Use dll first, but x is different, too much difference needed to check out the original one.

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from dataAnalysis import processor
 import numpy as np
 import os
 from ctypes import *
-import _ctypes
-import ctypes
+
+CUR_PATH = os.path.dirname(__file__)  # 获取当前运行脚本的绝对路径
+dllPath = os.path.join(CUR_PATH, "trilateration.dll")
+pDll = cdll.LoadLibrary(dllPath)
 
 
 class UWBMsg(Structure):
@@ -11,42 +13,53 @@ class UWBMsg(Structure):
                 ("y", c_double),
                 ("z", c_double)]
 
-location = UWBMsg()
-anchorArray = (UWBMsg*4)()
-distanceArray = (c_int*4)(-1)
+def trilateration(anchorPos, distances):
+# def trilateration():
 
+    location = UWBMsg()
+    anchorArray = (UWBMsg * 8)()
+    distanceArray = (c_int * 8)(-1)
 
-CUR_PATH=os.path.dirname(__file__)
-dllPath=os.path.join(CUR_PATH, "trilateration.dll")
-pDll=cdll.LoadLibrary(dllPath)
-result=0
+    for i in range(4):
+        anchorArray[i].x = anchorPos[i][0]
+        anchorArray[i].y = anchorPos[i][1]
+        anchorArray[i].z = anchorPos[i][2]
+        distanceArray[i] = distances[i]
 
-path = "./rawData/"
-anchorPos = [[0,0,0], [0.95,0,0], [0, 1.87, 0], [0.95, 1.87, 0]]
+    # 无效的测距值一定给 -1，否则会用随机数进行运算
+    distanceArray[4] = -1
+    distanceArray[5] = -1
+    distanceArray[6] = -1
+    distanceArray[7] = -1
 
-for i in range(4):
-    anchorArray[i].x = anchorPos[i][0]
-    anchorArray[i].y = anchorPos[i][1]
-    anchorArray[i].z = anchorPos[i][2]
+    result = pDll.GetLocation(byref(location), anchorArray, distanceArray)
+    return location.x, location.y, location.z
 
-walkPosition = [np.array([1.2, 0.49, 2.6 + (0.8 - 2.6) * i / 427]) for i in range(427)]
+# anchorPos = [[0,0,0], [0.95,0,0], [0, 1.87, 0], [0.95, 1.87, 0]]
+# distances = [2990, 3180, 2960, 2870]
+# print(trilateration(anchorPos, distances))
 
-target = processor(path=path, jsonName="6-16-40-10",anchorPos=anchorPos)
+# walkPosition = [np.array([1.2, 0.49, 2.6 + (0.8 - 2.6) * i / 427]) for i in range(427)]
 
 # MARK: Step 0 Check no.
 
 # MARK: Step 1 UWB position
+path = "./rawData/"
+anchorPos = [[0,0,0], [0.95,0,0], [0, 1.87, 0], [0.95, 1.87, 0]]
+target = processor(path=path, jsonName="6-16-40-10", anchorPos=anchorPos)
 target.getBLEmessage()
 distances = target.getDistancesFromBLEmessage()
 
-dllResults = []
+dllPositions = []
+for i in range(target.n):
+    x,y,z = trilateration(anchorPos, [int(distances[i][j] * 1000) for j in range(4)])
+    dllPositions.append([x,y,z])
 
-for i in range(len(distances)):
-    result = pDll.GetLocation(byref(location), anchorArray, distanceArray)
-    dllResults.append([location.x, location.y, location.z])
-print("dllResults: ", len(dllResults))
+print(dllPositions)
+target.Position2D(dllPositions, "dllPositions")
 
 uwbResults = target.calculateByDecawave(distances)
+target.Position2D(uwbResults, "uwbResults")
 # target.Position3D(uwbResults, "UWB", xlimInterval = [-1,1], ylimInterval=[1,2], zlimInterval=[-3,0])
 # backDistances = target.backCheckUWBResults(uwbResults)
 # target.draw4DistanceComparingGraphs(distances=distances, unknowDistances=backDistances)
